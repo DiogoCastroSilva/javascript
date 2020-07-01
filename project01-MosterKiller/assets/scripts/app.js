e345cccd733fb2a4b9d216d50f1882bf7e16658f
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-01 19:39:14 +0100

    Add heal functionality

--- project01-MosterKiller/assets/scripts/app.js
+++ project01-MosterKiller/assets/scripts/app.js
@@ -1,16 +1,76 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MOSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 
-let chossenMaxValue = 100;
+let chossenMaxLife = 100;
 let currentMosterHealth = chossenMaxValue;
-let currentHealth = chossenMaxValue;
+let currentPlayerHealth = chossenMaxValue;
 
 
-adjustHealthBars(chossenMaxValue);
+adjustHealthBars(chossenMaxLife);
 
-function attackHandler() {
+function attackMoster(mode = 'ATTACK') {
+    if (isGameOver()) {
+        return;
+    }
+    let maxDamage = ATTACK_VALUE;
+    if (mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
     const damage = dealMonsterDamage(ATTACK_VALUE);
     currentMosterHealth -= damage;
+    mosterAttack();
+    endRound();
+}
+
+function attackHandler() {
+    attackMoster('STRONG_ATTACK');
+}
+
+function strongAttackHandler() {
+    attackMoster();
+}
+
+function mosterAttack() {
+    const playerDamage = dealPlayerDamage(MOSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+}
+
+function healPlayerHandler() {
+    isGameOver();
+    let healValue = HEAL_VALUE;
+    if (currentPlayerHealth >= chossenMaxLife - HEAL_VALUE) {
+        healValue = chossenMaxLife - currentPlayerHealth;
+    }
+    increasePlayerHealth(healValue);
+    currentPlayerHealth += healValue;
+    mosterAttack();
+    endRound();
+}
+
+
+
+
+function isGameOver() {
+    if (currentPlayerHealth <= 0 || currentMosterHealth <= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function endRound() {
+    if (currentMosterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won');
+    } else if (currentPlayerHealth <= 0 && currentMosterHealth > 0) {
+        alert('You lost');
+    } else if (currentPlayerHealth <= 0 && currentMosterHealth <= 0) {
+        alert('You have a draw');
+    }
 }
 
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
