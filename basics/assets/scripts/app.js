d26179ac0921db125731e90ebdd677e79ff66ec5
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-07-01 08:47:04 +0100

    functions

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -1,7 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3;
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+
+currentResult = add(currentResult, 10) * 3;
 
 const calculationDescription = `(${defaultResult} + 10) * 3`;
 
