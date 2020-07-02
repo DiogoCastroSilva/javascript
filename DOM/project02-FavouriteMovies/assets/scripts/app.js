e7a078eb3b16aa1b514220fa1b27599bf8fb7216
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-02 22:58:54 +0100

    Add opening modal action

--- DOM/project02-FavouriteMovies/assets/scripts/app.js
+++ DOM/project02-FavouriteMovies/assets/scripts/app.js
@@ -1,3 +1,9 @@
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieBtn = document.querySelector('header button');
 
+
+
+
+startAddMovieBtn.addEventListener('click', () => {
+    addMovieModal.classList.toggle('visible');
+});
