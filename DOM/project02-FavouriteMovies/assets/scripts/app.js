a89c2679f8ab2b73ce10ae744674a18feb9b2f01
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-02 23:18:35 +0100

    Add movie handler

--- DOM/project02-FavouriteMovies/assets/scripts/app.js
+++ DOM/project02-FavouriteMovies/assets/scripts/app.js
@@ -1,9 +1,58 @@
 const addMovieModal = document.getElementById('add-modal');
+const cancelModalBtn = addMovieModal.querySelector('.btn--passive');
+const comfirmAddMovieBtn = cancelModalBtn.nextElementSibling;
 const startAddMovieBtn = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const userInputs = addMovieModal.querySelectorAll('input');
 
 
+const movies = [];
 
+const toogleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+};
 
-startAddMovieBtn.addEventListener('click', () => {
+const toogleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
-});
+    toogleBackdrop();
+};
+
+
+
+const backdropHandler = () => {
+    toogleMovieModal();
+};
+
+const cancelModalHandler = () => {
+    toogleMovieModal();
+};
+
+const addMovieHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if (
+        titleValue.trim() === '' &&
+        imageUrlValue.trim() === '' &&
+        +ratingValue < 1 ||
+        +ratingValue > 5
+    ) {
+        alert('Please enter a valid value!');
+        return;
+    }
+
+    const newMovie = {
+        title: titleValue,
+        image: imageUrlValue,
+        rating: ratingValue
+    };
+
+    movies.push(newMovie);
+};
+
+
+startAddMovieBtn.addEventListener('click', toogleMovieModal);
+backdrop.addEventListener('click', backdropHandler);
+cancelModalBtn.addEventListener('click', cancelModalHandler);
+comfirmAddMovieBtn.addEventListener('click', addMovieHandler);
