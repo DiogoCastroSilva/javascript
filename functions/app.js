22852e30465d52859b0ec2ae8e58fae91736258f
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-02 11:20:58 +0100

    Add user choices to the game

--- functions/app.js
+++ functions/app.js
@@ -1,16 +1,50 @@
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
+
+// function startGame() {
+//     console.log('Game is starting...');
+// }
+
+// or
+// const startGame = function start() {
+//     console.log('Game is starting...'); 
+// }
+// or
+// const startGame = function() {
+//     console.log('Game is starting...'); 
+// }
+// or
+const startGame = () => {
+    if (gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true;
     console.log('Game is starting...');
+    const playerSelection = getPlayerChoices();
+    
 }
 
-const person = {
-    name: 'Max',
-    greet: function greet() {
-        console.log('Hello there!');
+const getPlayerChoices = () => {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+    console.log(selection);
+    if (
+        selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS
+    ) {
+        alert('Invalid choice! We chosse Rock for you!');
+        return DEFAULT_CHOICE;
+    } else {
+        return selection;
     }
 };
 
-person.greet();
-
-startGameBtn.addEventListener('click', startGameBtn);
+startGameBtn.addEventListener('click', startGame);
