7e01b48c5b50c2515f53678cd04594f118eb7dca
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-02 14:34:37 +0100

    Using document methods to get DOM values

---
+++ DOM/app.js
@@ -0,0 +1,8 @@
+const h1 = document.getElementById('main-title');
+
+// Get all items with class
+const items = document.getElementsByClassName('list-item');
+// Get one item with class
+const item = document.querySelector('.list-item');
+// Get all items with class -> Node list
+const items2 = document.querySelectorAll('.list-item');
