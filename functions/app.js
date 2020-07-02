6ad523089cf4f32e11b969b2ef380d1201b4433d
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-02 11:48:15 +0100

    Adding game core functionality

--- functions/app.js
+++ functions/app.js
@@ -2,6 +2,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
@@ -22,17 +25,18 @@ const startGameBtn = document.getElementById('start-game-btn');
 //     console.log('Game is starting...'); 
 // }
 // or
-const startGame = () => {
+const startGameHandler = () => {
     if (gameIsRunning) {
         return;
     }
     gameIsRunning = true;
     console.log('Game is starting...');
-    const playerSelection = getPlayerChoices();
-    
-}
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    const winner = getWinner(computerSelection, playerSelection);
+};
 
-const getPlayerChoices = () => {
+const getPlayerChoice = () => {
     const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
     console.log(selection);
     if (
@@ -47,4 +51,29 @@ const getPlayerChoices = () => {
     }
 };
 
-startGameBtn.addEventListener('click', startGame);
+const getComputerChoice = () => {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+};
+
+const getWinner = (computerChoice, userChoice) => {
+    if (computerChoice === userChoice) {
+        return RESULT_DRAW;
+    } else if (
+        computerChoice === ROCK && userChoice === PAPER ||
+        computerChoice === SCISSORS && userChoice === ROCK ||
+        computerChoice === PAPER && userChoice === SCISSORS
+    ) {
+        return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
+};
+
+startGameBtn.addEventListener('click', startGameHandler);
