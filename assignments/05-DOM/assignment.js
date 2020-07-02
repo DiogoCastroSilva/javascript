be7c4256c13a608b37eddaf0f0d79d7e0fee10e5
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-02 15:04:36 +0100

    DOM assignment

---
+++ assignments/05-DOM/assignment.js
@@ -0,0 +1,19 @@
+// 01
+const task1 = document.getElementById('task-1');
+const task1Query = document.querySelector('#task-1');
+
+task1.style.backgroundColor = 'black';
+task1Query.style.color = 'white';
+
+// 02
+const title = document.querySelector('title');
+title.textContent = 'Assignment - Solved!';
+
+const head = document.head;
+const title2 = head.querySelector('title');
+title2.textContent = 'Assignment - Solved!';
+
+
+// 03
+const h1 = document.getElementsByTagName('h1')[0];
+h1.textContent = 'Assignment - Solved!';
