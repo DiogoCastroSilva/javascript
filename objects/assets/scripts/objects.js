33729d93da0537286b931199457db747f846c794
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-04 00:03:37 +0100

    Add movie funtionality

--- objects/assets/scripts/objects.js
+++ objects/assets/scripts/objects.js
@@ -1,3 +1,37 @@
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchMovieBtn = document.getElementById('search-btn');
+
+
+const movies = [];
+
+
+const addMovieHandler = () => {
+    const title = document.getElementById('title').value;
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+
+    if (
+        title.trim() === '' ||
+        extraName.trim() === '' ||
+        extraValue.trim() === ''
+    ) {
+        return;
+    }
+
+    const newMovie = {
+        info: {
+            title: title,
+            [extraName]: extraValue
+        },
+        id: new Date()
+    };
+
+    movies.push(newMovie);
+};
+
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchMovieBtn.addEventListener('click', () => {});
+
 // Recap
 // const person = {
 //     name: 'Max',
