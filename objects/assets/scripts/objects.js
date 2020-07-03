212f6ccf036b64ba09140cb70f6a61e55f427f96
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-03 23:55:25 +0100

    Object recap

---
+++ objects/assets/scripts/objects.js
@@ -0,0 +1,20 @@
+// Recap
+// const person = {
+//     name: 'Max',
+//     age: 30,
+//     hobbies: ['write', 'sports'],
+//     great: () => {
+//         console.log('Hi');
+//     }
+// };
+
+// // Acess properties
+// person.great() // 'Hi'
+
+// // Adding properties
+// person.isAdmin = true;
+// // Changing propeties
+// person.age = 31;
+// person['age'] = 32;
+// // Deleting
+// delete person.age;
