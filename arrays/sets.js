924fe853c1f4d553aa2c066b676d1f0d9c137718
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-07-03 17:01:54 +0100

    Working with Sets

---
+++ arrays/sets.js
@@ -0,0 +1,19 @@
+// Sets
+// Order not guaranteed and duplicates are not allowed, no index access
+const ids = new Set([1, 2, 3]);
+
+// Validate if set has value
+ids.has(1);
+
+// Adding values
+ids.add(1); // [1, 2, 3]
+ids.add(4); // [1, 2, 3, 4]
+
+// Removing
+ids.delete(1); // [2, 3]
+ids.delete(5); // [2, 3] -> No error
+
+// loop
+for(const entry of ids.entries()) {
+    console.log(entry);
+}
