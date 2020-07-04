aa04186ee2341519e2ca4df96f38f60beb41bb3e
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-04 10:37:02 +0100

    Create class section project

---
+++ classes/assets/scripts/app.js
@@ -0,0 +1,14 @@
+const products = [
+    {
+        title: 'A Pillow',
+        imageUrl: 'https://m.media-amazon.com/images/I/61wJMcvCKTL._SR500,500_.jpg',
+        price: 19.99,
+        description: 'A soft pillow.'
+    },
+    {
+        title: 'A Carpet',
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT7ddh0wE4hPXO5_3zQtVs94NmbP3o6TnWRYixT029aQDDZjO9iqEQaB6iBgA&usqp=CAc',
+        price: 600.97,
+        description: 'A carpet that you might like.'
+    }
+];
