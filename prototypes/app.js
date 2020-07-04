1d6446b87c2c767d99231f11d0fb2d1296d5aaf5
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-05 00:28:59 +0100

    Contructors

--- prototypes/app.js
+++ prototypes/app.js
@@ -1,11 +1,19 @@
-class Person {
-    name = 'Max';
-    constructor() {
-        this.age = 30;
-    }
+// class Person {
+//     name = 'Max';
+//     constructor() {
+//         this.age = 30;
+//     }
 
-    great() {
-        console.log('Hi!');
+//     great() {
+//         console.log('Hi!');
+//     }
+// }
+
+function Person() {
+    this.name = 'Max';
+    this.age = 30;
+    this.great = function() {
+        console.log('Hi');
     }
 }
 
