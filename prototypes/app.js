13cee085dc60ef01a4770c885c415563a44d987a
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-05 19:45:46 +0100

    Setting and getting Prototypes

--- prototypes/app.js
+++ prototypes/app.js
@@ -9,13 +9,45 @@
 //     }
 // }
 
-function Person() {
-    this.name = 'Max';
-    this.age = 30;
-}
+// function Person() {
+//     this.name = 'Max';
+//     this.age = 30;
+// }
 
-Person.prototype.great = function() {
-    console.log('Hi!');
-}
+// Person.prototype.great = function() {
+//     console.log('Hi!');
+// }
 
-const max = new Person();
+// const max = new Person();
+
+const course = {
+    title: 'Javascript - The complete guide',
+    rating: 5
+};
+
+Object.getPrototypeOf(course);
+Object.setPrototypeOf(course, {
+    printRating: function() {
+        console.log(`${this.rating}/5`);
+    }
+});
+
+const student = Object.create({
+    printProgress: function() {
+        console.log(this.progress);
+    }
+}, {
+    name: {
+        configurable: true,
+        enumerable: true,
+        value: 'Max',
+        writable: true
+    }
+});
+
+Object.defineProperty(student, 'progress', {
+    configurable: true,
+    enumerable: true,
+    value: 0.8,
+    writable: false
+});
