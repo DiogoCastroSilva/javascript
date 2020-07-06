f8d3ef59366f5a49f2d73dec53140a1e8a2eaa41
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-07-06 09:42:25 +0100

    Add logic to switch project arround from states active and finished

--- classes/project03-ProjectPlanner/assets/scripts/app.js
+++ classes/project03-ProjectPlanner/assets/scripts/app.js
@@ -1,19 +1,40 @@
+class DOMHelper {
+    static moveElement(id, destinationSelector) {
+        const element = document.getElementById(id);
+        const destinationElement = document.querySelector(destinationSelector);
+        destinationElement.append(element);
+    }
+
+    static clearEventListeners(element) {
+        const clonedElement = element.cloneNode(true);
+        element.replaceWith(clonedElement);
+        return clonedElement;
+    }
+}
+
 class Tooltip {}
 
 class ProjectItem {
-    constructor(id, updateProjectListsHandler) {
+    constructor(id, updateProjectListsHandler, type) {
         this.id = id;
         this.updateProjectListsHandler = updateProjectListsHandler;
         this.connectMoreInfoBtn();
-        this.connectSwitchBtn();
+        this.connectSwitchBtn(type);
     }
 
     connectMoreInfoBtn() {}
 
-    connectSwitchBtn() {
+    connectSwitchBtn(type) {
         const projectItemElement = document.getElementById(this.id);
-        const switchBtn = projectItemElement.querySelector('button:last-of-type');
-        switchBtn.addEventListener('click', this.updateProjectListsHandler);
+        let switchBtn = projectItemElement.querySelector('button:last-of-type');
+        switchBtn = DOMHelper.clearEventListeners(switchBtn);
+        switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
+        switchBtn.addEventListener('click', this.updateProjectListsHandler.bind(null, this.id));
+    }
+
+    update(newUpdateProjectListsHandler, type) {
+        this.updateProjectListsHandler = newUpdateProjectListsHandler;
+        this.connectSwitchBtn(type);
     }
 }
 
@@ -22,9 +43,9 @@ class ProjectCollection {
 
     constructor(type) {
         this.type = type;
-        const projectItems = document.querySelectorAll(`#${type}-project li`);
+        const projectItems = document.querySelectorAll(`#${type}-projects li`);
         for(const items of projectItems) {
-            this.projects.push(new ProjectItem(items.id, this.switchHandler.bind(this)));
+            this.projects.push(new ProjectItem(items.id, this.switchProject.bind(this), this.type));
         }
     }
 
@@ -32,7 +53,11 @@ class ProjectCollection {
         this.switchHandler = switchHandler;
     }
 
-    addProject() {}
+    addProject(project) {
+        this.projects.push(project);
+        DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+        project.update(this.switchProject.bind(this), this.type);
+    }
 
     switchProject(id) {
         this.switchHandler(this.projects.find(project => project.id === id));
