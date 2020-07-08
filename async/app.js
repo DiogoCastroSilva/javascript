2b37cec176e82120c55631825a6a9a8bc523ed8e
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-08 14:58:26 +0100

    Async calback operation

--- async/app.js
+++ async/app.js
@@ -2,14 +2,26 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 function trackUserHandler() {
-  console.log('Clicked!');
+  // Async operation
+  navigator.geolocation.getCurrentPosition(
+    data => {
+      console.log(data);
+    },
+    error => {
+      console.log(error);
+    }
+  );
+
+  // Will run first
+  console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
 
-let result = 0;
-for (let i = 0; i < 1000000000; i++) {
-  result += 1;
-}
-console.log(result);
-button.click(); // runs after the console.log so takes some time, Javascript is single thread
+// let result = 0;
+// for (let i = 0; i < 1000000000; i++) {
+//   result += 1;
+// }
+// console.log(result);
+// button.click(); // runs after the console.log so takes some time, Javascript is single thread
+
