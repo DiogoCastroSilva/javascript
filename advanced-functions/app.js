edf571bc24499381a4f2706c463c4037a26f615f
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-08 10:09:42 +0100

    Recursion

--- advanced-functions/app.js
+++ advanced-functions/app.js
@@ -51,6 +51,7 @@ const calculateIncomeTax = createTaxCalculator(0.25);
 console.log(calculateVATCalculator(100));
 console.log(calculateVATCalculator(200));
 
+
 // Closures
 let userName = 'Max';
 function greetUser() {
@@ -67,3 +68,70 @@ function greetUser1() {
 }
 let name = 'Max';
 greetUser(); // Hi Manuel
+
+
+// Recursion
+function powerOf(x, n) {
+    // Without Recursion
+    // let result = 1;
+
+    // for (let i = 0; i < n; i++) {
+    //     result *= x;
+    // }
+
+    // return result;
+
+    // With Recursion
+    // if (n === 1) {
+    //     return x;
+    // }
+    // return x * powerOf(x, n - 1);
+    // or
+    return n === 1 ? x : x * powerOf(x, n - 1);
+}
+
+console.log(powerOf(2, 3)); // 2 * 2 * 2
+
+// Advanced Recursion
+const myself = {
+    name: 'Max',
+    friends: [
+        {
+            name: 'Manuel',
+            friends: [
+                {
+                    name: 'Chris',
+                    friends: [
+                        {
+                            name: 'Ana',
+                            friends: [
+                                {
+                                    name: 'Joana'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Julia'
+        }
+    ]
+};
+
+function printFriendsNames(person) {
+    const collectedNames = [];
+
+    if (!person.friends) {
+        return [];
+    }
+    for (const friend of person.friends) {
+        collectedNames.push(friend.name);
+        collectedNames.push(...printFriendsNames(friend));
+    }
+
+    return collectedNames;
+}
+
+console.log(printFriendsNames(myself));
