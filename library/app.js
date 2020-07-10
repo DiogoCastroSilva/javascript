1bbcbd549b88fa1fe38af10f782bb9b931ea36fd
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-07-10 23:56:05 +0100

    Using axios

--- library/app.js
+++ library/app.js
@@ -1,4 +1,26 @@
 const custumers = ['Max', 'Manuel', 'Anna'];
 const activeCustumers = ['Max', 'Manuel'];
 
+// Lodash
 const inactiveCustumers = _.difference(custumers, activeCustumers);
+
+// Axios
+async function fetchPosts() {
+    try {
+        const response =  await axios.get(
+            'https://jsonplaceholder.typicode.com/posts'
+        );
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            throw response;
+        }
+    } catch(e) {
+        alert(e.message);
+    }
+    
+}
+
+fetchPosts().then(data => {
+    console.log(data);
+});
