69d0920ec6aca9bc20fc09836eae4b50fe3548e1
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-07-17 16:16:39 +0100

    Fallback for feature not availabel

--- browser-support/src/app.js
+++ browser-support/src/app.js
@@ -1,5 +1,20 @@
 const button = document.querySelector('button');
+const textParagraph = document.querySelector('p');
 
 button.addEventListener('click', () => {
-  // do something...
+  const text = textParagraph.textContent;
+
+  // Navigator not lways available
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(text)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(error => {
+    console.log(error);
+    });
+  } else {
+    alert('Feature not available! Please copy manually.');
+  }
+  
 });
