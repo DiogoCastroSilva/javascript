9f27e2214e758f1e543bc009b875b728eaaeb82f
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-07-17 08:55:20 +0100

    config development mode

--- workflows/webpack.config.js
+++ workflows/webpack.config.js
@@ -2,9 +2,14 @@ const path = require('path');
 
 // eslint-disable-next-line no-undef
 module.exports = {
+    mode: 'development',
     entry: './src/app.js',
     output: {
         filename: 'app.js',
-        path: path.resolve(__dirname, 'assets', 'scripts')
-    }
+        path: path.resolve(__dirname, 'assets', 'scripts'),
+        publicPath: 'assets/scripts/'
+    },
+    // devServer: {
+    //     contentBase: './'
+    // }
 };
