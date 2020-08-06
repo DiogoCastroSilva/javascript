36991542aac117eb42723ce2e8094b196fb8f142
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-08-06 15:37:20 +0100

    Understand Iterators

--- meta-programming/app.js
+++ meta-programming/app.js
@@ -14,3 +14,29 @@ person.id = 'p2'; // Should not be possible
 person[Symbol('uid')] = 'p2'; // Will add a new property, will not override uid Symbol
 
 Symbol('uid') === Symbol('uid') // false
+
+
+// Iterators
+const company = {
+    currentEmployee: 0,
+    employees: ['Diogo', 'Max', 'Anna'],
+    next() {
+        const returnValue = {
+            value: this.employees[this.currentEmployee],
+            done: false
+        };
+        if (this.currentEmployee >= this.employees.length) {
+            returnValue.done = false;
+            returnValue.value = this.currentEmployee;
+        } else {
+            this.currentEmployee++;
+        }
+        return returnValue
+    }
+};
+
+console.log(company.next());
+console.log(company.next());
+console.log(company.next());
+console.log(company.next());
+console.log(company.next());
