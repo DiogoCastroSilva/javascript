5de81c65c54bf8a805bc3aaac91322e293bb044c
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-08-18 18:50:22 +0100

    Micro optimizations

--- performance-optimizations/src/optimized/product-management.js
+++ performance-optimizations/src/optimized/product-management.js
@@ -1,26 +1,17 @@
 import { updateProdutc } from './rendering';
-import { products as prods } from './products';
+import { products } from './products';
 
-let products = prods;
+const titleEl = document.getElementById('title');
+const priceEl = document.getElementById('price');
 
 export function deleteProduct(prodId) {
-  const updatedProducts = [];
-  let deletedProd;
-  for (const prod of products) {
-    if (prod.id !== prodId) {
-      updatedProducts.push(prod);
-    } else {
-      deletedProd = prod;
-    }
-  }
-  products = updatedProducts;
+  const deleteProductIndex = products.findIndex(prod => prod.id === prodId);
+  const deletedProd = products[deleteProductIndex];
+  products.splice(deleteProductIndex, 1);
   updateProdutc(deletedProd, deleteProduct, false);
 }
 
 export function addProduct(event) {
-  const titleEl = document.querySelector('#new-product #title');
-  const priceEl = document.querySelector('#new-product #price');
-
   const title = titleEl.value;
   const price = priceEl.value;
 
