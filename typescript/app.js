fa33832322ffecba405266c2380bf64398886cc6
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-08-19 15:03:13 +0100

    Working with types and elements

--- typescript/app.js
+++ typescript/app.js
@@ -1,5 +1,12 @@
+var num1 = document.getElementById('num1');
+var num2 = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
 function add(a, b) {
     return a + b;
 }
-var result = add('2', 4);
-console.log(result);
+buttonElement.addEventListener('click', function () {
+    var num1Value = +num1.value;
+    var num2Value = +num2.value;
+    var result = add(num1Value, num2Value);
+    console.log(result);
+});
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,7 +1,17 @@
+const num1: HTMLInputElement = <HTMLInputElement>document.getElementById('num1');
+const num2: HTMLInputElement = <HTMLInputElement>document.getElementById('num2');
+const buttonElement: HTMLButtonElement = document.querySelector('button');
+
+
 function add(a: number, b: number): number {
     return a +b;
 }
 
-const result = add(2, 4);
 
-console.log(result);
+buttonElement.addEventListener('click', () => {
+    const num1Value = +num1.value;
+    const num2Value = +num2.value;
+
+    const result = add(num1Value, num2Value);
+    console.log(result);
+});
