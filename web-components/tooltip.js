c7e0434a4683c6b43a4ff3879f7480a9489e5fcd
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-08-19 15:43:50 +0100

    Adding events

--- web-components/tooltip.js
+++ web-components/tooltip.js
@@ -1,7 +1,30 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
-        console.log('custom element');
+        this.containerElement;
+    }
+
+    connectedCallback() {
+        const iconElement = document.createElement('span');
+        iconElement.textContent = ' (?)';
+
+        iconElement.addEventListener('mouseenter', this._showTooltip.bind(this));
+        iconElement.addEventListener('mouseleave', this._hideTooltip.bind(this));
+
+        this.appendChild(iconElement);
+    }
+
+    _showTooltip() {
+        this.containerElement = document.createElement('div');
+        this.containerElement.textContent = 'This is the tooltip text';
+
+        this.appendChild(this.containerElement);
+    }
+
+    _hideTooltip() {
+        if (this.containerElement) {
+            this.containerElement.remove();
+        }
     }
 }
 
